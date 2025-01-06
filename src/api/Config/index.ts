871e70d845512a1e17a3f11d6829092f13f96ca1
Babur <baburali894@gmail.com>
2025-01-06 11:27:20 +0500

    Fixed new issue

--- src/api/Config/index.ts
+++ src/api/Config/index.ts
@@ -2,16 +2,33 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 export const BASE_URL = "BASE_URL";
 
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: BASE_URL,
-});
-
-const api = {
-  request(config: AxiosRequestConfig = {}) {
-    return axiosInstance.request(config);
+export const newsAPIInstance: AxiosInstance = axios.create({
+  baseURL: "https://newsapi.org/v2",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer YOUR_NEWSAPI_KEY",
   },
-  get(url: string, config: AxiosRequestConfig = {}) {
-    return axiosInstance.get(`${url}`, config);
+});
+export const openNewsInstance: AxiosInstance = axios.create({
+  baseURL: "https://opennewsapi.com/v1",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer YOUR_OPENNEWS_API_KEY",
+  },
+});
+export const guardianInstance: AxiosInstance = axios.create({
+  baseURL: "https://content.guardianapis.com",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer YOUR_GUARDIAN_API_KEY",
+  },
+});
+const api = {
+  request(instance: AxiosInstance, config: AxiosRequestConfig = {}) {
+    return instance.request(config);
+  },
+  get(instance: AxiosInstance, url: string, config: AxiosRequestConfig = {}) {
+    return instance.get(`${url}`, config);
   },
   pageSize: 999999,
 };
