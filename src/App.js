ac8685ee703da1da12dab9504d9066650f3908f8
babur <baburali894@gmail.com>
2025-01-08 19:51:27 +0500

    Implement redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home/Index";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const App = () => {
   const queryClient = new QueryClient();
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div className="app">
-          <Home />
-        </div>
+        <Provider store={store}>
+          <div className="app">
+            <Home />
+          </div>
+        </Provider>
       </QueryClientProvider>
     </>
   );
